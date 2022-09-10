b95d087f1ae792934a8ca0ace7cbb7fad89d0d4d
Blechlawine <marczinser@gmx.de>
2022-09-10 20:58:43 +0200

    now remembers darkMode setting

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,7 +1,11 @@
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 export const toastText = ref<string | null>(null);
-export const darkMode = ref(false);
+export const darkMode = ref(localStorage.getItem("darkMode") === "true" ?? false);
+
+watch(darkMode, (newDarkMode) => {
+    localStorage.setItem("darkMode", newDarkMode.toString());
+});
 
 export const prevHighlightWidth = ref(0);
 export const prevHighlightMarginLeft = ref(0);
